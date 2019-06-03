94f195aa76f39458aac1970584753dc79b429715
jerostax <jeremy.geneste@gmail.com>
2019-06-03 17:48:09 +0200

    [ FEATURE ] Handling Language selection

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
 class App extends React.Component {
+  state = { language: 'français' };
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  }
+
   render() {
     return (
-    <div>App</div>
+    <div className='ui container'>
+      <div>
+          Séléctionnez un Langage:
+          <i className='flag fr' onClick={() => this.onLanguageChange('français')} />
+          <i className='flag us' onClick={() => this.onLanguageChange('english')} />
+      </div>
+      {this.state.language}
+    </div>
     );
   }
 }
