07aea9f22e0ca2cb9b23b0ac55736f89ac65ccc1
jerostax <jeremy.geneste@gmail.com>
2019-06-04 10:23:27 +0200

    [ FEATURE ] Added Context Provider

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
 
 class App extends React.Component {
   state = { language: 'français' };
 
   onLanguageChange = language => {
     this.setState({ language });
-  }
+  };
 
   render() {
     return (
@@ -16,7 +17,9 @@ class App extends React.Component {
           <i className='flag fr' onClick={() => this.onLanguageChange('français')} />
           <i className='flag us' onClick={() => this.onLanguageChange('english')} />
       </div>
-      <UserCreate />
+      <LanguageContext.Provider value={this.state.language} >
+        <UserCreate />
+      </LanguageContext.Provider>
     </div>
     );
   }
