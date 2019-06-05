6428688b1c16e193a7c8a126f20f667ac933a54a
jerostax <jeremy.geneste@gmail.com>
2019-06-05 10:27:26 +0200

    [ FEATURE ] Connected the LanguageSelector to the LanguageContext

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class LanguageSelector extends React.Component {
+  static contextType = LanguageContext;
+
   render() {
     return (
       <div>
           Séléctionnez un Langage:
-          <i className='flag fr' onClick={() => this.props.onLanguageChange('français')} />
-          <i className='flag us' onClick={() => this.props.onLanguageChange('english')} />
+          <i className='flag fr' onClick={() => this.context.onLanguageChange('français')} />
+          <i className='flag us' onClick={() => this.context.onLanguageChange('english')} />
       </div>
     );
   }
